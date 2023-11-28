8af22725e7b5f6b30f83a2351b7cc07613ca3445
Fredunio <michaldacko4@wp.pl>
2023-11-28 23:28:30 +0100

    Lab A done

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,85 @@
+import {Component, inject} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HousingLocationComponent} from "../housing-location/housing-location.component";
+import {HousingLocation} from "../housinglocation";
+import {HousingService} from "../housing.service";
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    CommonModule,
+    HousingLocationComponent,
+  ],
+
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+
+export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
+
+  constructor() {
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+
+}
+
+
+
+
+
+
+
+
+// export class HomeComponent {
+//   readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+//
+//   housingLocationList: HousingLocation[] = [
+//     {
+//       id: 0,
+//       name: 'Acme Fresh Start Housing',
+//       city: 'Chicago',
+//       state: 'IL',
+//       photo: `${this.baseUrl}/bernard-hermant-CLKGGwIBTaY-unsplash.jpg`,
+//       availableUnits: 4,
+//       wifi: true,
+//       laundry: true
+//     },
+//     {
+//       id: 1,
+//       name: 'A113 Transitional Housing',
+//       city: 'Santa Monica',
+//       state: 'CA',
+//       photo: `${this.baseUrl}/brandon-griggs-wR11KBaB86U-unsplash.jpg`,
+//       availableUnits: 0,
+//       wifi: false,
+//       laundry: true
+//     },
+//     {
+//       id: 2,
+//       name: 'Warm Beds Housing Support',
+//       city: 'Juneau',
+//       state: 'AK',
+//       photo: `${this.baseUrl}/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg`,
+//       availableUnits: 1,
+//       wifi: false,
+//       laundry: false
+//     },
+//   ]
+//
+//     }
